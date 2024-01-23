be6e37f4c5bcd85786122f3c120cecf6e63d986e
afshin-phpy <hassaniiafshin@gmail.com>
2024-01-24 02:36:27 +0330

    feat(user-auth): Add endpoint for user authentication
    - Repository added for database actions
    -  AuthController and Authservice added for getting user info

---
+++ app/Repositories/EloquentUserRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\Contracts\UserRepositoryInterface;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+
+class EloquentUserRepository implements UserRepositoryInterface
+{
+    public function create(array $data) : User
+    {
+        try {
+            return User::create($data);
+        } catch (\Throwable $th) {
+            throw new Exception($th->getMessage());
+        }
+    }
+
+    public function loginAttempt($credentials)
+    {
+        try {
+            return Auth::attempt($credentials);
+        } catch (\Throwable $th) {
+            throw new Exception($th->getMessage());
+        }
+    }
+
+    public function getAuthenticatedUser() : User
+    {
+        try {
+            return Auth::user();
+        } catch (\Throwable $th) {
+            throw new Exception($th->getMessage());
+        }
+    }
+}
